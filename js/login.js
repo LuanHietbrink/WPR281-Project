cec11467ffe94dfebf4b2a3eaffaefb6235f759c
LuanHietbrink <53099726+LuanHietbrink@users.noreply.github.com>
2022-08-27 00:38:01 +0200

    Kill me
    Created navbar for home page and tried but failed to get login page to work

--- js/login.js
+++ js/login.js
@@ -1,70 +1,18 @@
-
-(function ($) {
-    "use strict";
-
-
-    /*==================================================================
-    [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
+const username = "admin";
+const password = "ilovebugtracking1234";
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+const myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (document.getElementById("username") == "admin" && document.getElementById("pass") =="ilovebugtracking1234") {
+        window.location="http://127.0.0.1:5501/home.html";
     }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
+    else{
+        alert("Invalid username or password");
     }
+});
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
 
-})(jQuery);
